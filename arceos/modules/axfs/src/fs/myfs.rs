e4bf1b9619f6f9c877d8ea6e81c2cd41e5a1f5d9
GreenDay03 <1072224392@qq.com>
2023-05-29 00:43:17 +0800

    添加环境

--- arceos/modules/axfs/src/fs/myfs.rs
+++ arceos/modules/axfs/src/fs/myfs.rs
@@ -7,7 +7,47 @@ use axfs_vfs::VfsOps;
 其实就是从Github仓库https://github.com/pi-pi3/ext2-rs.git
 克隆下来，魔改一番再套层壳，测试一下就可以了。
 */
+use ext2::fs::{self, sync};
+use ext2::sector::Size512 as Sector;
+use ext2::volume::Volume;
 
+impl Volume<u8, Sector> for Disk {
+    type Error = ext2::error::Error;
+    //TODO：实现四个函数。
+    fn commit(
+            &mut self,
+            slice: Option<ext2::volume::VolumeCommit<u8, Sector>>,
+        ) -> Result<(), Self::Error> {
+        unimplemented!()
+    }
+    fn size(&self) -> ext2::volume::size::Size<Sector> {
+        unimplemented!()
+    }
+    fn slice<'a>(
+            &'a self,
+            range: core::ops::Range<ext2::sector::Address<Sector>>,
+        ) -> Result<ext2::volume::VolumeSlice<'a, u8, Sector>, Self::Error> {
+        unimplemented!()
+    }
+    unsafe fn slice_unchecked<'a>(
+            &'a self,
+            range: core::ops::Range<ext2::sector::Address<Sector>>,
+        ) -> ext2::volume::VolumeSlice<'a, u8, Sector> {
+        unimplemented!()
+    }
+}
+
+pub struct Ext2FileSystem {
+    inner : sync::Synced<fs::Ext2<Sector, Disk>>
+    // MAYBE something else
+}
+
+impl VfsOps for Ext2FileSystem {
+    //TODO 实现这个函数
+    fn root_dir(&self) -> axfs_vfs::VfsNodeRef {
+        unimplemented!()
+    }
+}
 /*
 实现结束！！！！
 */
