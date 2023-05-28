4319aa1956046ade67f1632637de5aaa2e407369
GreenDay03 <1072224392@qq.com>
2023-05-28 22:56:54 +0800

    增添文件系统的测试方法

--- arceos/modules/axfs/src/fs/myfs.rs
+++ arceos/modules/axfs/src/fs/myfs.rs
@@ -2,6 +2,16 @@ use crate::dev::Disk;
 use alloc::sync::Arc;
 use axfs_vfs::VfsOps;
 
+/*
+以下实现Ext文件系统！！！！！！！！！！！
+其实就是从Github仓库https://github.com/pi-pi3/ext2-rs.git
+克隆下来，魔改一番再套层壳，测试一下就可以了。
+*/
+
+/*
+实现结束！！！！
+*/
+
 /// The interface to define custom filesystems in user apps.
 #[crate_interface::def_interface]
 pub trait MyFileSystemIf {
