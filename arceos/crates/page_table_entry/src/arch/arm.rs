ea0f8b39cd9f74bb2e91b8161e1a60cdaccfeade
W34155552 <email@example.com>
2023-07-08 22:17:40 +0800

    finish pty

--- arceos/crates/page_table_entry/src/arch/arm.rs
+++ arceos/crates/page_table_entry/src/arch/arm.rs
@@ -12,14 +12,29 @@ bitflags::bitflags! {
         // Attribute fields in stage 1 VMSAv7-32 Page descriptors:
 
         /// Whether the descriptor is pagetable
-        const PT =       0b01 << 0;
+        //const PT =       0b01 << 0;
         /// Privileged execute-never (PXN) attribute
-        const PXN =   1 << 2;
+        //const PXN =   1 << 2;
         /// Non-secure bit
-        const NS =   1 << 3;
-        
+        //const NS =   1 << 3;
+        const XN_PVA=   1<<0;
+        const PTY=      1<<1;
+        const B=        1<<2;
+        const C=        1<<3;
+        const AP0=      1<<4;
+        const AP1=      1<<5;
+        const STEX=     0b111<<6;
+        const AP2=      1<<9;
+        const S=        1<<10;
+        const nG=       1<<11;
+
+
+        const BTEX=     0b111<<12;
+        const XN=       1<<15;
        
     }
+
+
 }
 
 #[repr(u32)]
@@ -31,43 +46,75 @@ enum MemType {
 
 impl DescriptorAttr {
     #[allow(clippy::unusual_byte_groupings)]
-    const ATTR_INDEX_MASK: u64 = 0b111_00;
+    const ATTR_INDEX_MASK: u32 = 0b111;
 
     const fn from_mem_type(mem_type: MemType) -> Self {
-        let mut bits = (mem_type as u64) << 2;
+        let mut bits:u32=Self::AP0.bits();
         if matches!(mem_type, MemType::Normal) {
-            bits |= Self::INNER.bits() | Self::SHAREABLE.bits();
+            bits|=7<<2;
+            bits|=1<<12;    
+            
+        }.
+        else{
+            bits|=1<<7;
+            bits|=1<<13;
         }
         Self::from_bits_truncate(bits)
     }
 
     fn mem_type(&self) -> MemType {
-        let idx = (self.bits() & Self::ATTR_INDEX_MASK) >> 2;
-        match idx {
-            0 => MemType::Device,
-            1 => MemType::Normal,
-            _ => panic!("Invalid memory attribute index"),
+        if self.contains(Self::PTY){
+            if !self.contains(Self::C) && !self.contains(Self::B) {
+                if (self.bits()>>6 & Self::ATTR_INDEX_MASK)==2{
+                    return MemType::Device
+                }
+            }
+            else if !self.contains(Self::C) && self.contains(Self::B){
+                if (self.bits()>>6 & Self::ATTR_INDEX_MASK)==0{
+                    return MemType::Device
+                }
+
+            }
         }
+            else{
+                if !self.contains(Self::C) && !self.contains(Self::B) {
+                    if (self.bits()>>6 & Self::ATTR_INDEX_MASK)==2{
+                        return MemType::Device
+                    }
+                }
+                else if !self.contains(Self::C) && self.contains(Self::B){
+                    if (self.bits()>>6 & Self::ATTR_INDEX_MASK)==0{
+                        return MemType::Device
+                    }
+    
+                }
+        }
+        return MemType::Normal
     }
 }
 
 impl From<DescriptorAttr> for MappingFlags {
     fn from(attr: DescriptorAttr) -> Self {
         let mut flags = Self::empty();
-        if attr.contains(DescriptorAttr::VALID) {
+        if attr.contains(DescriptorAttr::XN_PVA)||attr.contains(DescriptorAttr::PTY){
             flags |= Self::READ;
         }
-        if !attr.contains(DescriptorAttr::AP_RO) {
+        if !attr.contains(DescriptorAttr::AP2) {
             flags |= Self::WRITE;
         }
-        if attr.contains(DescriptorAttr::AP_EL0) {
+        if attr.contains(DescriptorAttr::AP1) {
             flags |= Self::USER;
-            if !attr.contains(DescriptorAttr::UXN) {
-                flags |= Self::EXECUTE;
+            if attr.contains(Self::PTY) {//smallpage
+                if !attr.contains(DescriptorAttr::XN_PVA){
+                    flags |= Self::EXECUTE;
+                }
             }
-        } else if !attr.intersects(DescriptorAttr::PXN) {
-            flags |= Self::EXECUTE;
-        }
+            else{
+                if !attr.contains(DescriptorAttr::XN){
+                    flags |= Self::EXECUTE;
+                }
+            }
+        } 
         if attr.mem_type() == MemType::Device {
             flags |= Self::DEVICE;
         }
@@ -83,21 +130,17 @@ impl From<MappingFlags> for DescriptorAttr {
             Self::from_mem_type(MemType::Normal)
         };
         if flags.contains(MappingFlags::READ) {
-            attr |= Self::VALID;
+            attr |= Self::PTY;
         }
         if !flags.contains(MappingFlags::WRITE) {
-            attr |= Self::AP_RO;
+            attr |= Self::AP2;
         }
         if flags.contains(MappingFlags::USER) {
-            attr |= Self::AP_EL0 | Self::PXN;
-            if !flags.contains(MappingFlags::EXECUTE) {
-                attr |= Self::UXN;
-            }
-        } else {
-            attr |= Self::UXN;
-            if !flags.contains(MappingFlags::EXECUTE) {
-                attr |= Self::PXN;
-            }
+            attr |= Self::AP1;
+            
+        } 
+        if !flags.contains(MappingFlags::EXECUTE) {
+                attr |= Self::XN_PVA | Self::PVA;
         }
         attr
     }
@@ -113,8 +156,8 @@ impl From<MappingFlags> for DescriptorAttr {
 pub struct A32PTE(u32);
 
 impl A32PTE {
-    const PHYS_ADDR_MASK: usize = 0xffff_f000; // bits 12..31
-
+    const PHYS_ADDR_SMASK: usize = 0xffff_f000; // bits 12..31
+    const PHYS_ADDR_BMASK: usize = 0xffff_0000; // bits 16..31
     /// Creates an empty descriptor with all bits set to zero.
     pub const fn empty() -> Self {
         Self(0)
@@ -122,33 +165,45 @@ impl A32PTE {
 }
 
 impl GenericPTE for A32PTE {
-    fn new_page(paddr: PhysAddr, flags: MappingFlags, is_huge: bool) -> Self {
-        let mut attr: DescriptorAttr = DescriptorAttr::from(flags) | DescriptorAttr::AF;
-        if !is_huge {
-            attr |= DescriptorAttr::NON_BLOCK;
+    fn new_page(paddr: PhysAddr, flags: MappingFlags, is_huge: bool) -> Self {//OK
+        let mut attr: DescriptorAttr = DescriptorAttr::from(flags);
+        if is_huge{
+            attr|=DescriptorAttr::XN_PVA;
+            attr&=~DescriptorAttr::STEX;
+            Self((attr.bits() & ~Self::PHYS_ADDR_BMASK) | (paddr.as_usize() & Self::PHYS_ADDR_MASK) as u32)
         }
-        Self(attr.bits() | (paddr.as_usize() & Self::PHYS_ADDR_MASK) as u64)
+        else{
+            Self((attr.bits() & ~Self::PHYS_ADDR_SMASK) | (paddr.as_usize() & Self::PHYS_ADDR_MASK) as u32)
+        }
+        
     }
-    fn new_table(paddr: PhysAddr) -> Self {
-        let attr = DescriptorAttr::NON_BLOCK | DescriptorAttr::VALID;
-        Self(attr.bits() | (paddr.as_usize() & Self::PHYS_ADDR_MASK) as u64)
+    fn new_table(paddr: PhysAddr) -> Self {//OK
+        let attr = DescriptorAttr::PTY;
+        Self(attr.bits() | (paddr.as_usize() & Self::PHYS_ADDR_SMASK) as u32)
     }
-    fn paddr(&self) -> PhysAddr {
-        PhysAddr::from(self.0 as usize & Self::PHYS_ADDR_MASK)
+    fn paddr(&self) -> PhysAddr {//OK
+        if GenericPTE::is_huge(self){
+            PhysAddr::from(self.0 as usize & Self::PHYS_ADDR_BMASK)
+        }
+        else{
+            PhysAddr::from(self.0 as usize & Self::PHYS_ADDR_SMASK)
+        }
     }
-    fn flags(&self) -> MappingFlags {
+    fn flags(&self) -> MappingFlags {//OK
         DescriptorAttr::from_bits_truncate(self.0).into()
     }
-    fn is_unused(&self) -> bool {
+    fn is_unused(&self) -> bool {//OK
         self.0 == 0
     }
-    fn is_present(&self) -> bool {
-        DescriptorAttr::from_bits_truncate(self.0).contains(DescriptorAttr::VALID)
+    fn is_present(&self) -> bool {//OK
+        let temp=DescriptorAttr::from_bits_truncate(self.0);
+        temp.contains(DescriptorAttr::XN_PVA)||temp.contains(DescriptorAttr::PTY)
     }
-    fn is_huge(&self) -> bool {
-        !DescriptorAttr::from_bits_truncate(self.0).contains(DescriptorAttr::NON_BLOCK)
+    fn is_huge(&self) -> bool {//OK
+        let temp=DescriptorAttr::from_bits_truncate(self.0);
+        temp.contains(DescriptorAttr::XN_PVA) && !temp.contains(DescriptorAttr::PTY)
     }
-    fn clear(&mut self) {
+    fn clear(&mut self) {//OK
         self.0 = 0
     }
 }
